312126320b7c452aabbe42275c16ac39e3f416f7
Yuri Skripal <yjura@protonmail.com>
2023-08-09 16:26:34 +0300

    Revert "feat: reset db"
    This reverts commit 4684bd629af21e6424cb6053d5534e1633eca0ce.

---
+++ prisma/migrations/20230804133041_dev/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "Album_name_key";
+
+-- DropIndex
+DROP INDEX "Artist_name_key";
+
+-- DropIndex
+DROP INDEX "Track_name_key";
+
+-- DropIndex
+DROP INDEX "User_login_key";
