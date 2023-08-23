97a3abcb7f3981c5e9d5334c6f98bbd9fdf6f8c0
Yuri Skripal <yjura@protonmail.com>
2023-08-23 14:18:59 +0300

    feat: return access token on login endpoint

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { ConfigModule } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 import { HashingService } from './hashing.service';
 import { UsersService } from 'src/users/users.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import jwtConfig from 'src/config/jwt.config';
 
 @Module({
   imports: [
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
     PrismaModule,
     PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET_KEY,
-      signOptions: { expiresIn: process.env.TOKEN_EXPIRE_TIME },
-    }),
   ],
   controllers: [AuthController],
   providers: [AuthService, HashingService, UsersService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,19 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ForbiddenException, Inject, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { HashingService } from 'src/auth/hashing.service';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigType } from '@nestjs/config';
+import jwtConfig from 'src/config/jwt.config';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly hashingService: HashingService,
     private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
   ) {}
 
   async signUp(authDto: AuthDto) {
@@ -28,7 +34,17 @@ export class AuthService {
 
     if (!isEqual)
       throw new ForbiddenException(`Password doesn\'t match actual one`);
+    const accessToken = await this.jwtService.signAsync(
+      {
+        sub: user.id,
+        login: user.login,
+      },
+      {
+        secret: this.jwtConfiguration.secret,
+        expiresIn: this.jwtConfiguration.tokenTtl,
+      },
+    );
 
-    return true;
+    return { accessToken };
   }
 }
